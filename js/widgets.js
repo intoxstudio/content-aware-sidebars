7ae62b87443a4f8481028605e359d71ffd1643ee
Joachim Jensen <jv@intox.dk>
2014-08-06 03:31:44 +0200

    adds widgets.js

---
+++ js/widgets.js
@@ -0,0 +1,42 @@
+/**
+ * @package Content Aware Sidebars
+ * @author Joachim Jensen <jv@intox.dk>
+ */
+
+(function($) {
+
+	var cas_admin = {
+
+		$sidebars: $(":not(.inactive) > div[id^='ca-sidebar']"),
+
+		init: function() {
+
+			this.addSidebarEditLink();
+
+		},
+
+		addSidebarEditLink: function() {
+
+			//When clicking on link, box should not close
+			$('.sidebar-name').on('click','.cas-edit-sidebar', function(e) {
+				e.stopPropagation();
+			});
+
+			this.$sidebars.each( function(e) {
+				$this = $(this);
+				var id = $this.attr('id').replace('ca-sidebar-','');
+				var $sidebar = $this.closest('.widgets-holder-wrap');
+
+				$sidebar.addClass('content-aware-sidebar')
+
+				$this.append('<div class="cas-sidebar-footer"><a title="'+CASAdmin.edit+'" class="cas-sidebar-link" href="post.php?post='+id+'&action=edit"><i class="dashicons dashicons-admin-generic"></i> '+CASAdmin.edit+'</a></div>');
+
+			});
+		}
+
+
+	}
+
+	$(document).ready(function(){ cas_admin.init(); });
+
+})(jQuery);
