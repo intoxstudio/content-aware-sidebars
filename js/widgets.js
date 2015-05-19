464051b9b47c37a5084913d818c3591ad9a913ed
Joachim Jensen <jv@intox.dk>
2015-05-19 00:41:39 -0700

    add ability to filter sidebars on widgets screen

--- js/widgets.js
+++ js/widgets.js
@@ -7,12 +7,49 @@
 
 	var cas_admin = {
 
-		$sidebars: $(":not(.inactive) > div[id^='ca-sidebar']"),
+		$container: $(".widget-liquid-right"),
+		$sidebars: $("div[id^='ca-sidebar']", this.$container),
 
 		init: function() {
 
 			this.addSidebarEditLink();
+			this.addFilterBox();
 
+		},
+
+		addFilterBox: function() {
+
+			var box = '<div class="wp-filter" style="margin: 10px 0px; vertical-align: middle;">'+
+			'<a href="post-new.php?post_type=sidebar" class="button button-primary" style="margin: 12px 0 11px">'+CASAdmin.addNew+'</a>'+
+  '<input type="search" class="js-cas-filter" placeholder="'+CASAdmin.filter+'..." style="margin: 12px 0 11px;float: right;">'+
+'</div>';
+
+			this.$container.prepend(box);
+			this.filterListener();
+
+		},
+
+		filterListener: function() {
+			var that = this, filterTimer, cachedFilter = "";
+			this.$container.on('keyup', '.js-cas-filter',function(e) {
+				var filter = $(this).val();
+				if(filter != cachedFilter) {
+					cachedFilter = filter;
+					if( filterTimer ) {
+						clearTimeout(filterTimer);
+					}
+					filterTimer = setTimeout(function(){
+						$(".widgets-holder-wrap",that.$container).each(function(key,sidebar) {
+							var $sidebar = $(sidebar);
+							if ($sidebar.find("h3").first().text().search(new RegExp(filter, "i")) < 0) {
+								$sidebar.fadeOut();
+							} else {
+								$sidebar.fadeIn();
+							}
+						});
+					}, 250);
+				}
+			});
 		},
 
 		addSidebarEditLink: function() {
