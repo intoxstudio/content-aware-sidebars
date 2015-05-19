c796da4f4936c58550f6c1f50984bbb5c5345341
Joachim Jensen <jv@intox.dk>
2015-05-19 00:43:16 -0700

    uninstall script makes use of condition group syncing

--- uninstall.php
+++ uninstall.php
@@ -13,8 +13,14 @@ global $wpdb;
 // Remove db version
 delete_option('cas_db_version');
 
-//Remove sidebars, sidebar groups and (if not null) their terms and metadata
-$wpdb->query("DELETE p.*,pm.*,tr.* FROM $wpdb->posts AS p LEFT JOIN $wpdb->postmeta AS pm ON p.ID = pm.post_id LEFT JOIN $wpdb->term_relationships AS tr ON p.ID = tr.object_id WHERE p.post_type = 'sidebar' OR p.post_type = 'sidebar_group'");
+//Remove all sidebars, meta and terms. Synced with condition groups
+$sidebars = get_posts(array(
+	"post_type" => "sidebar",
+	"posts_per_page" => -1
+));
+foreach ($sidebars as $sidebar) {
+	wp_delete_post($sidebar->ID,true);
+}
 
 // Remove user meta
 $wpdb->query("DELETE FROM $wpdb->usermeta WHERE meta_key IN('metaboxhidden_sidebar','closedpostboxes_sidebar')");
