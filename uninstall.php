c6ca0453bf05216ae8b93ef15129738f37451da6
Joachim Jensen <jv@intox.dk>
2015-10-01 15:05:03 -0700

    properly delete condition groups on uninstall. delete other residual data as well

--- uninstall.php
+++ uninstall.php
@@ -13,14 +13,22 @@ global $wpdb;
 // Remove db version
 delete_option('cas_db_version');
 
-//Remove all sidebars, meta and terms. Synced with condition groups
+//Remove all sidebars, groups, meta and terms.
 $sidebars = get_posts(array(
-	"post_type" => "sidebar",
+	"post_type"      => "sidebar",
 	"posts_per_page" => -1
 ));
 foreach ($sidebars as $sidebar) {
+	$groups = get_posts(array(
+		"post_parent"    => $sidebar->ID,
+		"post_type"      => "condition_group",
+		"posts_per_page" => -1
+	));
+	foreach($groups as $group) {
+		wp_delete_post($group->ID,true);
+	}
 	wp_delete_post($sidebar->ID,true);
 }
 
 // Remove user meta
-$wpdb->query("DELETE FROM $wpdb->usermeta WHERE meta_key IN('metaboxhidden_sidebar','closedpostboxes_sidebar')");
+$wpdb->query("DELETE FROM $wpdb->usermeta WHERE meta_key IN('metaboxhidden_sidebar','closedpostboxes_sidebar','managesidebarcolumnshidden','".$wpdb->prefix."_ca_cas_tour')");
