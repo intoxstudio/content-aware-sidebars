60903d142792ec8ed39aabe0d4a7153775f1ae00
Joachim Jensen <jv@intox.dk>
2017-04-04 18:45:15 +0200

    styling on widget manager

--- js/widgets.js
+++ js/widgets.js
@@ -25,25 +25,28 @@
 			this.addWidgetSearch();
 			this.toggleSidebarStatus();
 
-			var $widget_list = $('#widget-list');
+			// var $widget_list = $('.widget-liquid-left');
 
-			$('div.widgets-sortables')
-			.on('sortstart',function(e,ui) {
-				console.log("YO");
-				$widget_list.css('overflow-y','visible');
-			})
-			.on('sortstop',function(e,ui) {
-				$widget_list.css('overflow-y','auto');
-			});
+			// $('div.widgets-sortables')
+			// .on('sortstart',function(e,ui) {
+			// 	console.log("start");
+			// 	$widget_list.css('overflow-y','hidden');
+			// })
+			// .on('sortstop',function(e,ui) {
+			// 	console.log("stop");
+			// 	$widget_list.css('overflow-y','auto');
+			// });
 
 			
-			$('#widget-list').children('.widget')
-			.on('dragstart',function(e,ui) {
-				$widget_list.css('overflow-y','visible');
-			})
-			.on('dragstop',function(e,ui) {
-				$widget_list.css('overflow-y','auto');
-			});
+			// $widget_list.children('.widget')
+			// .on('dragstart',function(e,ui) {
+			// 	console.log("start 2");
+			// 	$widget_list.css('overflow-y','hidden');
+			// })
+			// .on('dragstop',function(e,ui) {
+			// 	console.log("stop 2");
+			// 	$widget_list.css('overflow-y','auto');
+			// });
 
 		},
 
