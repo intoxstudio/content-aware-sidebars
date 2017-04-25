733ada7b3c38bab3c8070bc99b6579490b1f3a2a
Joachim Jensen <jv@intox.dk>
2017-04-25 13:44:45 +0200

    use abspath instead of own const

--- admin/post_type_sidebar.php
+++ admin/post_type_sidebar.php
@@ -6,9 +6,7 @@
  * @copyright 2017 by Joachim Jensen
  */
 
-if (!defined('CAS_App::PLUGIN_VERSION')) {
-	header('Status: 403 Forbidden');
-	header('HTTP/1.1 403 Forbidden');
+if (!defined('ABSPATH')) {
 	exit;
 }
 
