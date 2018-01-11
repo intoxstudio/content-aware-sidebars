c4d530464f121787e14e50bd11c87d1ca1a56812
Joachim Jensen <jv@intox.dk>
2018-01-10 22:21:34 -0800

    sidebar list: only show arrow on merge action

--- admin/sidebar-list-table.php
+++ admin/sidebar-list-table.php
@@ -447,7 +447,7 @@ class CAS_Sidebar_List_Table extends WP_List_Table {
 					$return = $action->get_list_data($post->ID);
 					$host = $metadata->get('host')->get_list_data($post->ID);
 					$return .= ": " . ($host ? $host : '<span style="color:red;">' . __('Please update Host Sidebar', "content-aware-sidebars") . '</span>');
-					if($action->get_data($post->ID) != 3) {
+					if($action->get_data($post->ID) == 1) {
 						$pos = $metadata->get("merge_pos")->get_data($post->ID,true);
 						$pos_icon = $pos ? "up" : "down";
 						$pos_title = array(
