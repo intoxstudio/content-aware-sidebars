9f0788f716eb20a3b6840369be871eeaf6919764
Joachim Jensen <jv@intox.dk>
2018-02-08 16:15:39 -0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const less = require('gulp-less');
 const uglify = require('gulp-uglify');
 const rename = require("gulp-rename");
 const zip = require("gulp-zip");
+const freemius = require("gulp-freemius-deploy");
+const fs_config = require( './fs-config.json' );
 
 gulp.task('less', function (done) {
 	return gulp.src('css/style.less')
@@ -35,9 +37,21 @@ gulp.task('uglify', function () {
 });
 
 gulp.task('zip', function() {
-	return gulp.src(['**','!{build{,/**}','!node_modules{,/**}'],{base:'../'})
+	return gulp.src(['**','!build{,/**}','!**/node_modules{,/**}'],{base:'../'})
 		.pipe(zip('content-aware-sidebars.zip'))
 		.pipe(gulp.dest('build'));
+});
+
+gulp.task('freemius', function() {
+	return freemius( gulp, {
+		developer_id   : fs_config.developer_id,
+		plugin_id      : fs_config.plugin_id,
+		public_key     : fs_config.public_key,
+		secret_key     : fs_config.secret_key,
+		zip_name       : 'content-aware-sidebars.zip',
+		zip_path       : 'build/',
+		add_contributor: true
+	});
 });
 
 gulp.task('watch', function() {
@@ -47,7 +61,7 @@ gulp.task('watch', function() {
 
 gulp.task('build', gulp.parallel('less','uglify'));
 
-gulp.task('deploy', gulp.serial('build','zip'));
+gulp.task('deploy', gulp.series('build','zip'));
 
 gulp.task('default', gulp.parallel('build'));
 
