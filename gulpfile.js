8dfe40e0660c43b25a6edf8cba9f7f8217c85331
Joachim Jensen <jv@intox.dk>
2020-11-03 23:37:13 -0800

    freemius step for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ const rename = require("gulp-rename");
 const replace = require("gulp-replace");
 const cleanCSS = require("gulp-clean-css");
 const zip = require("gulp-zip");
-const freemius = require("gulp-freemius-deploy");
 const fs_config = require( './fs-config.json' );
 const pkg = require('./package.json');
 
@@ -48,14 +47,14 @@ gulp.task('uglify', function () {
 gulp.task('zip', function () {
 	return gulp.src([
 		'**',
+		'!fs-config.json',
 		'!build{,/**}',
-		'!**/node_modules{,/**}',
-		'!**/package{,-lock}.json',
+		'!**/package*.json',
 		'!**/pnpm-lock.yaml',
 		'!**/*.{less,scss,po,pot,js}',
 		'!**/{scss,less}{,/**}',
-		'!fs-config.json',
 		'{lib/freemius/assets/js/*,lib/wp-pointer-tour/assets/js/*,**/*.min}.js',
+		'!**/node_modules{,/**}'
 	], { base: '../' })
 		.pipe(zip(pkg.name + '.zip'))
 		.pipe(gulp.dest('build'));
@@ -68,17 +67,15 @@ gulp.task('update-version', function (done) {
 		.pipe(gulp.dest('./'));
 });
 
-gulp.task('freemius', function() {
-	return freemius( gulp, {
-		developer_id   : fs_config.developer_id,
-		plugin_id      : fs_config.plugin_id,
-		public_key     : fs_config.public_key,
-		secret_key     : fs_config.secret_key,
-		zip_name       : 'content-aware-sidebars.zip',
-		zip_path       : 'build/',
-		add_contributor: true
-	});
-});
+require( 'aelia-gulp-freemius-deploy' )( gulp, {
+    developer_id: fs_config.developer_id,
+    plugin_id: fs_config.plugin_id,
+    public_key: fs_config.public_key,
+    secret_key: fs_config.secret_key,
+    zip_name: pkg.name + '.zip',
+    zip_path: 'build/',
+    add_contributor: true
+} );
 
 gulp.task('watch', function() {
 	gulp.watch('assets/css/style.less', gulp.parallel('less'));
@@ -86,5 +83,5 @@ gulp.task('watch', function() {
 });
 
 gulp.task('build', gulp.parallel('less','uglify'));
-gulp.task('deploy', gulp.series('build','zip'));
+gulp.task('deploy', gulp.series('build','zip', 'aelia-gulp-freemius-deploy'));
 gulp.task('default', gulp.parallel('build'));
