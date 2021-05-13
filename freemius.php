3cab348cb353d678a5aa7c456c5c154f985294cb
Joachim Jensen <jv@intox.dk>
2021-05-12 23:17:45 -0700

    do not run full uninstall on pro

--- freemius.php
+++ freemius.php
@@ -99,16 +99,18 @@ function cas_fs_upgrade()
 }
 add_action('admin_init', 'cas_fs_upgrade', 999);
 
-function cas_fs_uninstall()
-{
-    require(plugin_dir_path(__FILE__).'/cas_uninstall.php');
-}
+if (!$cas_fs->can_use_premium_code()) {
+    function cas_fs_uninstall()
+    {
+        require(plugin_dir_path(__FILE__).'/cas_uninstall.php');
+    }
 
-if ($cas_fs->is_on()) {
-    $cas_fs->add_action('after_uninstall', 'cas_fs_uninstall');
-} elseif (is_admin()) {
-    //after_uninstall is only run for new users
-    register_uninstall_hook(plugin_dir_path(__FILE__).'content-aware-sidebars.php', 'cas_fs_uninstall');
+    if ($cas_fs->is_on()) {
+        $cas_fs->add_action('after_uninstall', 'cas_fs_uninstall');
+    } elseif (is_admin()) {
+        //after_uninstall is only run for new users
+        register_uninstall_hook(plugin_dir_path(__FILE__).'content-aware-sidebars.php', 'cas_fs_uninstall');
+    }
 }
 
 if ($cas_fs->is__premium_only()) {
